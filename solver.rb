c74576d197ea83223d09ae5c385b6599fa0b09c8
dlwub <dlwubi@gmail.com>
2022-11-17 13:44:09 +0300

    Add reverse method of Solver class

--- solver.rb
+++ solver.rb
@@ -1,7 +1,17 @@
 class Solver
-  def factorial(num)
-    return 1 if num.zero?
+	def factorial(num)
+		return 1 if num.zero?
 
-    num * factorial(num - 1)
-  end
+		num * factorial(num - 1)
+	end
+
+	def reverse(str)
+		last = str.length - 1
+    result=''
+    for i in 0..last do      
+      result+=str[last-i]       
+    end
+    result
+
+	end    
 end
