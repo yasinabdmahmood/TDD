bcfc012c9375c889b33b3d372a7a7981a925ed37
yasinabdmahmood <abd181103@gmail.com>
2022-11-17 12:33:46 +0300

    Fix rubocup errors

--- solver.rb
+++ solver.rb
@@ -1,8 +1,7 @@
 class Solver
-    def factorial(n)
-        if n==0
-            return 1
-        end
-        return n*factorial(n-1)
-    end
+  def factorial(num)
+    return 1 if num.zero?
+
+    num * factorial(num - 1)
+  end
 end
--- solver_rspec.rb
+++ solver_rspec.rb
@@ -1,19 +1,19 @@
 require './solver'
 
 describe Solver do
-    context 'Test factorial method' do
-        solver = Solver.new
-        it 'factorial of zero is equal to one' do          
-            factorial = solver.factorial(0)
-            expect(factorial).to eql 1
-        end
-        it 'factorial of 4 is equal to 24' do          
-            factorial = solver.factorial(4)
-            expect(factorial).to eql 24
-        end
-        it 'factorial of 6 is equal to 720' do          
-            factorial = solver.factorial(6)
-            expect(factorial).to eql 720
-        end
+  context 'Test factorial method' do
+    solver = Solver.new
+    it 'factorial of zero is equal to one' do
+      factorial = solver.factorial(0)
+      expect(factorial).to eql 1
     end
+    it 'factorial of 4 is equal to 24' do
+      factorial = solver.factorial(4)
+      expect(factorial).to eql 24
+    end
+    it 'factorial of 6 is equal to 720' do
+      factorial = solver.factorial(6)
+      expect(factorial).to eql 720
+    end
+  end
 end
